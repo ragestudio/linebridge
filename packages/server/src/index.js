6f12edfb6997a5d1d5440e1637f0aca6690ecdfd
srgooglo <chippotato39@gmail.com>
2021-05-31 14:13:41 +0200

    added headers & origin resolver

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -1,10 +1,13 @@
 const axios = require("axios")
+const express = require("express")
+const { objectToArrayMap } = require("@corenode/utils")
+const uuid = require("uuid")
 
 const http = require("http")
 const wsServer = require('websocket').server
 const wsFrame = require('websocket').frame
 
-
+const SERVER_VERSION = runtime.helpers.getVersion()
 class Controller {
     constructor(key, exec, params) {
         this.params = params
@@ -13,24 +16,36 @@ class Controller {
             this.exec = exec
         }
     }
-    
+
     exec(req, res) {
         res.send(`Im alive!`)
         console.log(`This is an default controller function`)
     }
 }
 
-class Server {
+class RequestServer {
     constructor(params) {
         this.params = params ?? {}
 
         this.endpoints = {}
         this.endpointsAddress = []
+        this.routes = []
+
+        this.headers = {
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+            "Access-Control-Allow-Credentials": "true"
+        }
         
         this._everyRequest = null
         this._onRequest = {}
 
         this.httpServer = require("express")()
+
+        if (typeof this.params.port === "undefined") {
+            this.params.port = 3010
+        }
 
         if (this.params.autoInit) {
             this.init()
@@ -42,7 +57,7 @@ class Server {
             this._everyRequest = context
         }
     }
-    
+
     onRequest = (key, context) => {
         if (typeof key === "undefined") {
             return false
@@ -53,10 +68,14 @@ class Server {
     }
 
     registerEndpoint(method, route, controller) {
-        route = `/${route}`
+        if (typeof controller === "function") {
+            controller = new Controller(route, controller)
+        }
+
+        this.routes.push(route)
         this.endpoints[route] = { method: method, route: route, controller: controller }
         this.endpointsAddress.push(this.endpoints[route])
-        
+
         this.httpServer[method](route, (req, res) => this.httpRequest(req, res, this.endpoints[route]))
     }
 
@@ -78,19 +97,41 @@ class Server {
     }
 
     init() {
+        this.httpServer.use(express.json())
+        this.httpServer.use(express.urlencoded({ extended: true }))
+
+        this.httpServer.use((req, res, next) => {
+            objectToArrayMap(this.headers).forEach((entry) => {
+                res.setHeader(entry.key, entry.value)
+            })
+
+            next()
+        })
+
         // todo: read endponts.json and itterate
 
-        this.httpServer.listen(3010, () => {
-            console.log(`Ready!`)
+        // register root resolver
+        this.registerEndpoint("get", "/", (req, res) => {
+            // here server origin resolver
+            res.json({
+                uuid: uuid.v4(),
+                originID: this.params.oid ?? "RelicServer",
+                version: SERVER_VERSION,
+                routes: this.routes
+            })
+        })
+
+        this.httpServer.listen(this.params.port, () => {
+            console.log(`Ready on port ${this.params.port}!`)
         })
     }
 }
 
-export { Controller, Server }
+module.exports = { Controller, Server: RequestServer }
 
 // create default server
-const defServer = new Server({ autoInit: true })
-
+const defServer = new RequestServer({ autoInit: true })
+defServer.onRequest()
 
 
 
