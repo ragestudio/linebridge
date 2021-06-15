b39948a0a99d58985a990eb815e002eb8251f699
srgooglo <chippotato39@gmail.com>
2021-06-15 17:41:19 +0200

    register

--- packages/server/src/lib/nethub/index.js
+++ packages/server/src/lib/nethub/index.js
@@ -29,7 +29,7 @@ function heartbeat() {
 }
 
 async function registerOrigin(payload) {
-    putRegistry({ data: { ...payload } })
+    putRegistry({ ...payload })
         .then((res) => {
             console.log(res.data)
         })
