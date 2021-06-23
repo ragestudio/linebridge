1f921b53b5895ed5a4069d3a68188cc17f10c6cb
srgooglo <chippotato39@gmail.com>
2021-06-23 14:39:37 +0200

    remove client bridge

--- bin/server.js
+++ bin/server.js
@@ -2,10 +2,7 @@ const cloudlink = require("../dist")
 const random = require("corenode/dist/libs/random")
 
 // create server
-const server = new cloudlink.Server({
+new cloudlink.Server({
     autoInit: true,
-    id: runtime.args.id ?? random.generateName()
+    id: runtime.args.id ?? random.generateName(),
 })
-
-// connect
-const client = cloudlink.Client.createInterface(server.localOrigin)
