eec835b6e8cafe3eef6ee04d546d72370652e81f
srgooglo <srgooglo@ragestudio.net>
2021-07-08 10:49:55 +0200

    added modules.d.ts

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 const path = require('path')
 
 //* set globals
-global.runtime = runtime
-global.IS_DEV = runtime.helpers.isDevMode()
+global.runtime = process.runtime
+global.IS_DEV = process.runtime.helpers.isDevMode()
 global.RELIC_ORIGIN = require('./relicOrigin.json')
-global.SERVER_VERSION = runtime.helpers.getVersion()
+global.SERVER_VERSION = process.runtime.helpers.getVersion()
 global.SERVER_MANIFEST = "server.manifest"
 global.SERVER_MANIFEST_PATH = path.resolve(process.cwd(), SERVER_MANIFEST)
-
-runtime.registerModulesAliases({
-    "@classes": path.resolve(__dirname, 'classes'),
-})
 
 //* import libraries
 const Classes = require('./classes')
---
+++ src/modules.d.ts
@@ -0,0 +1,12 @@
+declare module 'cloudlink' {
+  export namespace client {
+    export class Bridge {};
+    export function createInterface(address: string): Promise<object>;
+  }
+  export namespace server {
+
+  }
+  export namespace classes {
+
+  }
+}
