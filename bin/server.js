a494d8e0d5a2473dded51f8276624cf69126aa43
srgooglo <srgooglo@ragestudio.net>
2021-09-24 08:30:34 +0200

    moved bin to src

--- bin/server.js
+++
@@ -1,8 +0,0 @@
-const cloudlink = require("../dist")
-const random = require("corenode/dist/libs/random")
-
-// create server
-new cloudlink.Server({
-    autoInit: true,
-    id: runtime.args.id ?? random.generateName(),
-})
