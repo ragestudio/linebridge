1614d80ab0e35e2b9b2bc08c58911dd1060e495f
srgooglo <srgooglo@ragestudio.net>
2022-02-23 13:55:59 +0100

    reimplement `attach` `detach` methods

--- src/classes/ClientWSInterface/index.js
+++ src/classes/ClientWSInterface/index.js
@@ -4,22 +4,40 @@ module.exports = class WSInterface {
     constructor(params = {}) {
         this.params = params
         this.manager = new io.Manager(this.params.origin, {
-            autoConnect: true,
+            autoConnect: false,
             transports: ["websocket"],
             ...this.params.managerOptions,
         })
         this.sockets = {}
 
-        this.register("/", "main")
+        this.attach("/", "main", this.params.mainSocketOptions)
     }
 
-    register = (socket, as) => {
+    attach = (socket, as, options) => {
         if (typeof socket !== "string") {
             console.error("socket must be string")
             return false
         }
 
-        socket = this.manager.socket(socket)
+        socket = this.manager.socket(socket, options)
+
         return this.sockets[as ?? socket] = socket
+    }
+
+    detach = (socketName) => {
+        if (typeof socketName !== "string") {
+            console.error("socketName must be string")
+            return false
+        }
+        if (typeof this.sockets[socketName] === "undefined") {
+            console.error("socket not found")
+            return false
+        }
+
+        if (this.sockets[socketName].connected) {
+            this.sockets[socketName].disconnect()
+        }
+
+        delete this.sockets[socketName]
     }
 }
