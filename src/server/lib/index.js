8f0b96d12599895926e4b20bfb9ecbc14df6e302
srgooglo <srgooglo@ragestudio.net>
2022-05-13 11:39:47 +0200

    update exports

--- src/server/lib/index.js
+++ src/server/lib/index.js
@@ -1,4 +1,5 @@
 module.exports = {
     serverManifest: require("./serverManifest"),
     outputServerError: require("./outputServerError"),
+    resolveRemoteEvents: require("./resolveRemoteEvents"),
 }
