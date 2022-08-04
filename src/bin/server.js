53c112d7c459084c5a59d698f0ccab0df5bbc591
srgooglo <srgooglo@ragestudio.net>
2022-08-04 16:52:19 +0200

    use async

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env EXPERIMENTAL_FASTCALL=1 node
 const corenode = require("corenode")
 
-corenode.runInNewRuntime(() => {
+corenode.runInNewRuntime(async () => {
     const { Server } = require("../server/index.js")
 
     const instance = new Server({
         id: process.env.serverID,
     })
 
-    instance.initialize()
+    await instance.initialize()
 })
