b595fcec422c5e26e980aca3058863d68449c9a2
SrGooglo <srgooglo@ragestudio.net>
2023-05-11 18:11:04 +0200

    added name to example server

--- example_server/src/index.js
+++ example_server/src/index.js
@@ -3,6 +3,7 @@ import { registerBaseAliases, Server } from "../../src/server"
 registerBaseAliases()
 
 const server = new Server({
+    name: "example_server",
     listen_port: 3011,
 },
     require("@controllers"),
