bb2453ad76a5e9810d35838cb730bdca4692bda6
SrGooglo <srgooglo@ragestudio.net>
2024-05-09 21:36:43 +0000

    remove controller

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     Server: require("./server.js"),
-    Controller: require("./classes/controller"),
     Endpoint: require("./classes/endpoint"),
     registerBaseAliases: require("./registerAliases"),
     version: require("../../package.json").version,
