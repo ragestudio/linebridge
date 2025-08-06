0e174ef07a39a9bbfb47cefa7b2cf3e5ecb246b4
srgooglo <srgooglo@ragestudio.net>
2025-08-06 22:24:46 +0200

    implement ws request handlers

---
+++ server/src/classes/Handler/WebsocketRequestHandler.js
@@ -0,0 +1,47 @@
+export default class WebsocketRequestHandler {
+	constructor(engine, params) {
+		this.engine = engine
+		this.params = params
+
+		// apply contexts
+		if (Array.isArray(this.params.useContexts)) {
+			for (const key of this.params.useContexts) {
+				this.ctx[key] = this.engine.server.contexts[key]
+			}
+		}
+	}
+
+	ctx = {}
+
+	execute = async (client, payload) => {
+		let result = null
+		let error = null
+
+		try {
+			result = await this.params.fn(client, payload.data, {
+				...this.ctx,
+				...payload,
+			})
+		} catch (err) {
+			if (!(err instanceof OperationError)) {
+				console.log(`[ws] 500 ${this.params.event} >`, err)
+			}
+
+			error = err
+		}
+
+		// handle ack mode
+		if (payload.ack === true) {
+			client.socket.send(
+				this.engine.encode({
+					event: `ack_${this.params.event}`,
+					error: error?.message ?? null,
+					data: result,
+				}),
+			)
+		}
+
+		// return the result
+		return result
+	}
+}
