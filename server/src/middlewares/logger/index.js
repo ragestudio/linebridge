fcc957b954b739fac4e901b0b6e454e3787fdbf8
srgooglo <srgooglo@ragestudio.net>
2025-08-06 22:26:29 +0200

    format

--- server/src/middlewares/logger/index.js
+++ server/src/middlewares/logger/index.js
@@ -1,20 +1,22 @@
 export default (req, res, next) => {
-    const startHrTime = process.hrtime()
+	const startHrTime = process.hrtime()
 
-    res.on("finish", () => {
-        let url = req.url
-        const elapsedHrTime = process.hrtime(startHrTime)
-        const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6
+	res.on("finish", () => {
+		let url = req.url
+		const elapsedHrTime = process.hrtime(startHrTime)
+		const elapsedTimeInMs = elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6
 
-        res._responseTimeMs = elapsedTimeInMs
+		res._responseTimeMs = elapsedTimeInMs
 
-        // cut req.url if is too long
-        if (url.length > 100) {
-            url = url.substring(0, 100) + "..."
-        }
+		// cut req.url if is too long
+		if (url.length > 100) {
+			url = url.substring(0, 100) + "..."
+		}
 
-        console.log(`${req.method} ${res._status_code ?? res.statusCode ?? 200} ${url} ${elapsedTimeInMs}ms`)
-    })
+		console.log(
+			`${req.method} ${res._status_code ?? res.statusCode ?? 200} ${url} ${elapsedTimeInMs}ms`,
+		)
+	})
 
-    next()
+	next()
 }
