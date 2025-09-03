4354afa2f88e49c278535a92fe86b603d3e3cbc3
srgooglo <srgooglo@ragestudio.net>
2025-09-03 13:56:41 +0200

    Format service ipc registration payload

--- server/src/registers/ipcService.js
+++ server/src/registers/ipcService.js
@@ -25,9 +25,7 @@ export default async (server) => {
 
 	process.send({
 		type: "service:register",
-		id: process.env.lb_service.id,
-		index: process.env.lb_service.index,
-		register: {
+		data: {
 			namespace: server.params.refName,
 			http: {
 				enabled: true,
@@ -35,7 +33,7 @@ export default async (server) => {
 				proto: server.hasSSL ? "https" : "http",
 			},
 			websocket: {
-				enabled: server.params.websockets,
+				enabled: server.params.websockets?.enabled ?? false,
 				path: server.params.refName ?? `/${server.params.refName}`,
 			},
 			listen: {
