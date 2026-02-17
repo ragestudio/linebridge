ae93a9159282e9a06cc7abbd00ca2f33abe53564
srgooglo <srgooglo@ragestudio.net>
2026-02-17 15:29:50 +0100

    Delete local gateway code

--- gateway/core/websocket/on_pong.go
+++
@@ -1,7 +0,0 @@
-package websocket
-
-import (
-	"github.com/lxzan/gws"
-)
-
-func (manager *Instance) OnPong(socket *gws.Conn, payload []byte) {}
--- gateway/structs/EventData.go
+++
@@ -1,6 +0,0 @@
-package structs
-
-type EventData struct {
-	Event string `json:"event"`
-	Data  any    `json:"data,omitempty"`
-}
