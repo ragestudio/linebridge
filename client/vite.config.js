c4f8cb851053e759ad7d5d62f198e7dd0b12aa12
srgooglo <srgooglo@ragestudio.net>
2026-03-21 17:46:50 +0100

    Bump client version and switch to Vite

---
+++ client/vite.config.js
@@ -0,0 +1,12 @@
+import { dirname, resolve } from "node:path"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: [resolve(import.meta.dirname, "src/index.js")],
+			name: "linebridge-client",
+			fileName: "index",
+		},
+	},
+})
