815777fc1c37978779736e00054d89db94a17913
srgooglo <srgooglo@ragestudio.net>
2026-06-04 22:16:47 +0200

    Update baseHeaders.ts

--- server/src/registers/baseHeaders.ts
+++ server/src/registers/baseHeaders.ts
@@ -2,24 +2,14 @@ import Vars from "../vars"
 import type Server from "../server"
 
 export default (server: Server): void | null => {
-	if (!server || !server.headers || !server.engine) {
+	if (!server || !server.engine) {
 		return null
 	}
 
-	let headers = {
+	const baseHeaders = {
 		...server.headers,
 		...Vars.baseHeaders,
 	}
 
-	const headerEntries = Object.entries(headers)
-
-	server.engine.register_middleware(
-		(req: any, res: any, next: () => void) => {
-			for (let i = 0; i < headerEntries.length; i++) {
-				res.setHeader(headerEntries[i][0], headerEntries[i][1])
-			}
-
-			next()
-		},
-	)
+	server.engine.baseHeaders = Object.entries(baseHeaders)
 }
