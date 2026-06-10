f5d912061af89a8689e1664d3928cb59b9bdee38
srgooglo <srgooglo@ragestudio.net>
2026-06-10 23:02:31 +0200

    Update nav.ts

--- docs/src/.vitepress/nav.ts
+++ docs/src/.vitepress/nav.ts
@@ -2,4 +2,8 @@ export default [
 	{ text: "Guide", link: "/guide/getting-started" },
 	{ text: "API Reference", link: "/api/server" },
 	{ text: "Ultragateway", link: "/ultragateway/" },
+	{
+		text: "2.0.0alpha",
+		items: [{ text: "2.0.0alpha", link: "/" }],
+	},
 ]
